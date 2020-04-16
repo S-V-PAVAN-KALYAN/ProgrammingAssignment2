7f3c5818dde38cc3551d4516b952a6fbfdb39a94
S-V-PAVAN-KALYAN <58887871+S-V-PAVAN-KALYAN@users.noreply.github.com>
2020-04-16 13:59:44 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## creates a special "matrix" object that can cache its inverse.
 
-## Write a short comment describing this function
+makeCacheMatrix <- function(x = numeric())
+{
 
-makeCacheMatrix <- function(x = matrix()) {
+        inv <- NULL
 
+        get <- function() x
+
+        getInverse <- function() 
+                {
+
+                if(length(x)%%sqrt(length(x))==0) ## make sure matrix is square
+                        { 
+
+                        inv <<- solve(x)
+                }
+        }
+
+        list(get = get, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## computes the inverse of the special "matrix" returned by 
+
+## makeCacheMatrix above. If the inverse has already been calculated 
+
+## and the matrix has not changed), then the cachesolve should retrieve 
+
+## the inverse from the cache.
+
+cacheSolve <- function(x, ...)
+        {
+
+        if(!is.atomic(x)) # check if the given parameter is atomic
+        {      
+                inv <- x$getInverse()
+
+                if(!is.null(inv))
+                {
+
+                        message("getting cached data")
+
+                        return(inv)
+                }
+        }
+ else  # if not atomic then make it
+ {        
+                message("getting the inverse-- no cached data found")
+
+                return(makeCacheMatrix(x)$getInverse())
+ } 
 }
